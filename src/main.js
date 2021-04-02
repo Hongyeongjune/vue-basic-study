22111faf8556e60c6a1fed9a18db9199ebac5918
hongyeongjune <hongyeongjune@naver.com>
2021-04-02 11:42:15 +0900

    Vuex 기초 state 관리

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VuexApp from "@/vuex/VuexApp"
+import { store } from "./store"
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store: store,
+  render: h => h(VuexApp),
+}).$mount('#vuex-app')
---
+++ src/vuex/VuexApp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="vuex-app">
+    Parent counter : {{ $store.state.counter }} <br />
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+
+    <child v-bind:num="counter"></child>
+  </div>
+</template>
+
+<script>
+
+import Child from "./Child";
+export default {
+  name: 'VuexApp',
+  components: {
+    Child
+  },
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    addCounter() {
+      this.$store.state.counter++;
+    },
+    subCounter() {
+      this.$store.state.counter--;
+    }
+  }
+}
+</script>
+
+<style>
+#vuex-app {
+
+}
+</style>
